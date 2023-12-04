dd36566ce94766b8e701fdc9e5a8c62c90d8f379
Kadgar782 <bogdan7823@mail.ru>
2023-12-04 17:19:19 +0600

    Improved the appearance of the accordions

--- app/components/FAQSection.tsx
+++ app/components/FAQSection.tsx
@@ -1,104 +1,27 @@
+"use client";
 import "./details.css";
-import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
-import { ChevronUp } from "lucide-react";
+import React from "react";
+import { CustomAccordion } from "./accordion";
 
 export const FAQ = () => {
+  const LorumIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Suspendisse malesuada lacus ex, sit amet blandit leo loborti seget.";
+
   return (
-    <section className="flex w-4/5 flex-row justify-around gap-4 ">
+    <section className="flex w-4/5 flex-row justify-around gap-4 pt-36 ">
       <section className="flex flex-col text-text">
-        <h1> FAQ</h1>
-        <h2> Answers to some questions you might have.</h2>
+        <h1 className="pb-10 text-5xl">
+          <strong>FAQ</strong>
+        </h1>
+        <h2 className="text-2xl"> Answers to some questions you might have.</h2>
       </section>
       <section className="flex w-3/5 flex-col">
-        <Accordion className="relative mb-8 box-border cursor-pointer rounded-md  bg-secondary text-text">
-          <AccordionSummary
-            expandIcon={<ChevronUp />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            sx={{
-              backgroundColor: "#240800",
-              borderRadius: "10px",
-              borderColor: "#240800",
-            }}
-          >
-            <h1 className="text-2xl text-text ">Accordion 1</h1>
-          </AccordionSummary>
-          <AccordionDetails sx={{ backgroundColor: "#240800" }}>
-            <p className="bg-secondary text-lg text-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </p>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion className="relative mb-8 box-border cursor-pointer rounded-md  bg-secondary text-text">
-          <AccordionSummary
-            expandIcon={<ChevronUp />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            sx={{ backgroundColor: "#240800", borderRadius: "10px" }}
-          >
-            <h1 className="text-2xl text-text ">Accordion 1</h1>
-          </AccordionSummary>
-          <AccordionDetails sx={{ backgroundColor: "#240800" }}>
-            <p className="bg-secondary text-lg text-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </p>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion className="relative mb-8 box-border cursor-pointer rounded-md  bg-secondary text-text">
-          <AccordionSummary
-            expandIcon={<ChevronUp />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            sx={{ backgroundColor: "#240800", borderRadius: "10px" }}
-          >
-            <h1 className="text-2xl text-text ">Accordion 1</h1>
-          </AccordionSummary>
-          <AccordionDetails sx={{ backgroundColor: "#240800" }}>
-            <p className="bg-secondary text-lg text-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </p>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion className="relative mb-8 box-border cursor-pointer rounded-md  bg-secondary text-text">
-          <AccordionSummary
-            expandIcon={<ChevronUp />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            sx={{ backgroundColor: "#240800", borderRadius: "10px" }}
-          >
-            <h1 className="text-2xl text-text ">Accordion 1</h1>
-          </AccordionSummary>
-          <AccordionDetails sx={{ backgroundColor: "#240800" }}>
-            <p className="bg-secondary text-lg text-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </p>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion className="relative mb-8 box-border cursor-pointer rounded-md  bg-secondary text-text">
-          <AccordionSummary
-            expandIcon={<ChevronUp />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-            sx={{ backgroundColor: "#240800", borderRadius: "10px" }}
-          >
-            <h1 className="text-2xl text-text ">Accordion 1</h1>
-          </AccordionSummary>
-          <AccordionDetails sx={{ backgroundColor: "#240800" }}>
-            <p className="bg-secondary text-lg text-text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-              eget.
-            </p>
-          </AccordionDetails>
-        </Accordion>
+        <CustomAccordion accordionName="First" accordionContent={LorumIpsum} />
+        <CustomAccordion accordionName="Second" accordionContent={LorumIpsum} />
+        <CustomAccordion accordionName="Third" accordionContent={LorumIpsum} />
+        <CustomAccordion accordionName="Fourth" accordionContent={LorumIpsum} />
+        <CustomAccordion accordionName="Fifth" accordionContent={LorumIpsum} />
+        <CustomAccordion accordionName="Sixth" accordionContent={LorumIpsum} />
       </section>
     </section>
   );
---
+++ app/components/accordion.tsx
@@ -0,0 +1,58 @@
+"use client";
+import "./details.css";
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import { ChevronUp } from "lucide-react";
+import React from "react";
+import { useState } from "react";
+
+export interface SingleAccordion {
+  accordionName: string;
+  accordionContent: string;
+}
+
+export const CustomAccordion = ({
+  accordionName,
+  accordionContent,
+}: SingleAccordion) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  //this is necessary because otherwise both summary and details will have borderRadius in the accordion and it will not look very good
+  const handleAccordionChange = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <Accordion
+      className="relative mb-8 box-border  cursor-pointer rounded-md bg-bacgroundCard text-text"
+      sx={{ backgroundColor: "#623527", boxShadow: 0 }}
+      onChange={handleAccordionChange}
+      expanded={isOpen}
+    >
+      <AccordionSummary
+        expandIcon={<ChevronUp stroke="#439eb6" />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+        sx={{
+          backgroundColor: "#240800",
+          borderTopLeftRadius: "10px",
+          borderTopRightRadius: "10px",
+          //if the accordion is open, the borderRadius will be removed from the bottom of the Summary
+
+          borderBottomLeftRadius: isOpen ? 0 : "10px",
+          borderBottomRightRadius: isOpen ? 0 : "10px",
+        }}
+      >
+        <h1 className="text-2xl text-text ">{accordionName}</h1>
+      </AccordionSummary>
+      <AccordionDetails
+        sx={{
+          backgroundColor: "#240800",
+          borderBottomLeftRadius: "10px",
+          borderBottomRightRadius: "10px",
+        }}
+      >
+        <p className="bg-secondary text-lg text-text">{accordionContent}</p>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
