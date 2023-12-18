7aa05a3a969dcc564af2a3190bad30d779cd27d8
Kadgar782 <bogdan7823@mail.ru>
2023-12-18 18:44:01 +0600

    Added an expand icon to the accordion

--- app/components/accordion.tsx
+++ app/components/accordion.tsx
@@ -14,6 +14,27 @@ export interface SingleAccordion {
   accordionContent: string;
 }
 
+function Icon(open: boolean) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={`${
+        open ? "rotate-180" : ""
+      } mr-2 h-8 w-8  transition-transform`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+      />
+    </svg>
+  );
+}
+
 export const CustomAccordion = ({
   accordionName,
   accordionContent,
@@ -27,6 +48,7 @@ export const CustomAccordion = ({
 
   return (
     <Accordion
+      icon={Icon(isOpen)}
       open={isOpen}
       className=" relative mb-8 box-border cursor-pointer rounded-lg border border-secondary bg-secondary bg-clip-border px-1 text-text"
     >
