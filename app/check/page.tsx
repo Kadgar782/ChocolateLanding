25a11e4160c938300284709adcc19c8a90069484
Kadgar782 <bogdan7823@mail.ru>
2024-03-25 15:35:14 +0500

    &apos; fix

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <section className="flex h-screen w-full items-center justify-center bg-background pb-20 text-text">
       <h1>
-        If it wasn't a mockup, there would be a payment for the product in the
-        cart
+        If it wasn&apos;t a mockup, there would be a payment for the product in
+        the cart
       </h1>
     </section>
   );
