eb8e73d36646f484b551cf02a4613135fc89fa0e
Kadgar782 <bogdan7823@mail.ru>
2024-03-25 15:20:28 +0500

    added placeholder

---
+++ app/check/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section className="flex h-screen w-full items-center justify-center bg-background pb-20 text-text">
+      <h1>
+        If it wasn't a mockup, there would be a payment for the product in the
+        cart
+      </h1>
+    </section>
+  );
+}
